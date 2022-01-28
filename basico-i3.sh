655baf7fa6ebc44b5078111daad1a882bab25823
brascubas1869 <97002733+brascubas1869@users.noreply.github.com>
2022-01-28 14:08:05 -0300

    Add files via upload

---
+++ basico-i3.sh
@@ -0,0 +1,30 @@
+sudo apt install build-essential \
+                 apt-listchanges \
+                 apt-listbugs \
+                 curl \
+                 lynx \
+                 i3 \
+                 xorg \
+                 lightdm \
+                 lxappearance \
+                 feh \
+                 pcmanfm \
+                 qutebrowser \
+                 pulseaudio \
+                 alsa-utils \
+                 pavucontrol \
+                 rofi \
+                 vim \
+                 picom \
+                 network-manager \
+                 blueman \
+                 htop \
+                 papirus-icon-theme \
+                 arc-theme \
+                 youtube-dl \
+                 vlc \
+                 geany
+
+
+
+
---
+++ pos-install.sh
@@ -0,0 +1,9 @@
+sudo apt install youtube-dl \
+                 ranger \
+                 polybar \
+                 rxvt-unicode \
+                 xsel \
+                 qbittorrent \
+                 redshift \
+                 breeze-cursor-theme
+
